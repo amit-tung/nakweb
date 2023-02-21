a3361324a067e84919bb3311fab8e3fa50100f22
amit-tung <amit@nicktung.com>
2023-02-21 17:59:23 +0530

    TEstsdvx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,6 @@ export async function getServerSideProps(context:any) {
   try {
     await clientPromise
     // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
 
     return {
       props: { isConnected: true },
